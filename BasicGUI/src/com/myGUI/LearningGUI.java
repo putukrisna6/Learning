3863b99aa6b4489331be0fcc8a64d9ffb32369f4
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2020-10-07 19:18:33 +0700

    Update LearningGUI.java

--- BasicGUI/src/com/myGUI/LearningGUI.java
+++ BasicGUI/src/com/myGUI/LearningGUI.java
@@ -2,21 +2,37 @@ package com.myGUI;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class LearningGUI {
+public class LearningGUI implements ActionListener{
+	
+	private int count = 0;
+	private JLabel label;
 	
 	public LearningGUI() {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
-		panel.setLayout(new GridLayout(0, 1));
 		
-		//add panel to the frame, 
+		JButton button = new JButton("Click it!");
+		button.addActionListener(this);
+		
+		label = new JLabel("Number of clicks: 0");
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 10, 20));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		//add panel to the frame, make it exit on close, set the title, set the window to a certain size, set to make it visible
 		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Epic GUI");
 		frame.pack(); 
 		frame.setVisible(true);
@@ -24,6 +40,12 @@ public class LearningGUI {
 	
 	public static void main(String[] args) {
 		new LearningGUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count);
 	}
 
 }
