265388fa9be3c839f39ba36a6ac84f750f20dd01
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2020-10-21 11:25:20 +0700

    Update Main.java

--- Tugas Week 4/HourlyEmployee/src/com/main/Main.java
+++ Tugas Week 4/HourlyEmployee/src/com/main/Main.java
@@ -4,8 +4,23 @@ import com.employee.*;
 public class Main {
 
 	public static void main(String[] args) {
-		HourlyEmployee myHour = new HourlyEmployee("Jill", "Cook", "123-90-000", 60.0, 3.0);
-		System.out.println(myHour.toString());
+		HourlyEmployee hEmpl = new HourlyEmployee("Jill", "Cook", "123-90-000", 60.0, 3.0);
+		
+		//get hourly employee data;
+		System.out.println("Information obtained by get methods: ");
+		System.out.printf("%n%s %s%n", "First name is", hEmpl.getFirstName());
+		System.out.printf("%s %s%n", "Last name is", hEmpl.getLastName());
+		System.out.printf("%s %s%n", "SSN is", hEmpl.getSocialSecurityNumber());
+		System.out.printf("%s %s%n", "Hourly wage is", hEmpl.getWage());
+		System.out.printf("%s %s%n", "Hours worked is", hEmpl.getHours());
+
+		//modify
+		hEmpl.setHours(120.0);
+		hEmpl.setWage(24);
+		
+		//get data from toString()
+		System.out.println("\nInformation obtained by toString() method\n");
+		System.out.println(hEmpl.toString());
 	}
 
 }
